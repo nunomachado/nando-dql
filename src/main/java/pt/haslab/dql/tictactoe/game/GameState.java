231bbb4dace306d0c2f5f07c6b3711f708b457fe
GitButler <gitbutler@gitbutler.com>
2024-07-16 16:06:32 +0200

    refactor: Remove Java files

--- src/main/java/pt/haslab/dql/tictactoe/game/GameState.java
+++
@@ -1,8 +0,0 @@
-package pt.haslab.dql.tictactoe.game;
-
-/**
- * Created by nunomachado on 05/08/17.
- */
-public enum GameState {
-    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
-}
--- src/main/java/pt/haslab/dql/tictactoe/game/Seed.java
+++
@@ -1,8 +0,0 @@
-package pt.haslab.dql.tictactoe.game;
-
-/**
- * Created by nunomachado on 05/08/17.
- */
-public enum Seed {
-    EMPTY, CROSS, NOUGHT
-}
